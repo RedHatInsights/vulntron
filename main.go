2c24aeebc483a22887d37a2039d1804402d51c88
Gabor Burges <gburges@redhat.com>
2023-01-30 21:15:19 +0100

    consumergroupified

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
 	"os/exec"
 
-	"github.com/Shopify/sarama"
+	kafka "github.com/Shopify/sarama"
 	_ "github.com/lib/pq"
 )
 
@@ -17,6 +18,30 @@ const (
 	password = "mypassword"
 	dbname   = "mydb"
 )
+
+var imageTag string
+
+// ConsumerGroupHandler represents a Sarama consumer group consumer
+type ConsumerGroupHandler struct{}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (ConsumerGroupHandler) Setup(kafka.ConsumerGroupSession) error { return nil }
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (ConsumerGroupHandler) Cleanup(kafka.ConsumerGroupSession) error { return nil }
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (ConsumerGroupHandler) ConsumeClaim(session kafka.ConsumerGroupSession, claim kafka.ConsumerGroupClaim) error {
+	// loop over the messages in the claim
+	for message := range claim.Messages() {
+		fmt.Printf("Message topic:%q partition:%d offset:%d value:%s\n",
+			message.Topic, message.Partition, message.Offset, string(message.Value))
+		session.MarkMessage(message, "")
+		imageTag = string(message.Value)
+	}
+
+	return nil
+}
 
 func main() {
 	// Connect to the PostgreSQL database
@@ -31,50 +56,49 @@ func main() {
 	// Get the Quay image tag from the command line (for practice)
 	// imageTag := os.Args[1]
 
+	config := kafka.NewConfig()
+	config.Consumer.Offsets.Initial = kafka.OffsetOldest
 	brokers := []string{os.Getenv("KAFKA_BROKER")}
-	topic := os.Getenv("KAFKA_TOPIC")
+	topic := []string{os.Getenv("KAFKA_TOPIC")}
+	consumergroup := "vulntron"
+	ctx := context.Background()
 
-	consumer, err := sarama.NewConsumer(brokers, nil)
+	consumer, err := kafka.NewConsumerGroup(brokers, consumergroup, config)
 	if err != nil {
 		fmt.Println("Error creating consumer:", err)
 		return
 	}
 	defer consumer.Close()
 
-	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
-	if err != nil {
-		fmt.Println("Error creating partition consumer:", err)
-		return
-	}
-	defer partitionConsumer.Close()
-
 	for {
-		select {
-		case msg := <-partitionConsumer.Messages():
-			imageTag := string(msg.Value)
-			fmt.Println("Received image tag:", imageTag)
+		handler := ConsumerGroupHandler{}
 
-			// Run Syft on the image
-			syftOut, err := exec.Command("syft", "--scope", "all-layers", fmt.Sprint(imageTag), "-o", "json=syftout.json").Output() // #nosec G204
-			if err != nil {
-				fmt.Println("Error running Syft:", err)
-				return
-			}
-
-			// Run Grype on the image
-			grypeOut, err := exec.Command("grype", "--scope", "all-layers", fmt.Sprint(imageTag), "-o", "json", "--file", "grypeout.json").Output() // #nosec G204
-			if err != nil {
-				fmt.Println("Error running Grype:", err)
-				return
-			}
-
-			// Insert the results into the "scan_results" table
-			_, err = db.Exec("INSERT INTO scan_results (image_tag, syft_output, grype_output) VALUES ($1, $2, $3)", imageTag, string(syftOut), string(grypeOut))
-			if err != nil {
-				fmt.Println("Error inserting results into the database:", err)
-				return
-			}
-			fmt.Println("Results stored in the database.")
+		err := consumer.Consume(ctx, topic, handler)
+		if err != nil {
+			fmt.Println("Error consuming:", err)
+			return
 		}
+
+		// Run Syft on the image
+		syftOut, err := exec.Command("syft", "--scope", "all-layers", fmt.Sprint(imageTag), "-o", "json=syftout.json").Output() // #nosec G204
+		if err != nil {
+			fmt.Println("Error running Syft:", err)
+			return
+		}
+
+		// Run Grype on the image
+		grypeOut, err := exec.Command("grype", "--scope", "all-layers", fmt.Sprint(imageTag), "-o", "json", "--file", "grypeout.json").Output() // #nosec G204
+		if err != nil {
+			fmt.Println("Error running Grype:", err)
+			return
+		}
+
+		// Insert the results into the "scan_results" table
+		_, err = db.Exec("INSERT INTO scan_results (image_tag, syft_output, grype_output) VALUES ($1, $2, $3)", imageTag, string(syftOut), string(grypeOut))
+		if err != nil {
+			fmt.Println("Error inserting results into the database:", err)
+			return
+		}
+		fmt.Println("Results stored in the database.")
 	}
 }
